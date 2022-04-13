5560589b3fc9ffaedde19c3ac155f89050dd935f
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2022-04-13 13:48:19 -0500

    add gradle and initial test library

---
+++ src/main/java/tigerlib/Library.java
@@ -0,0 +1,10 @@
+package tigerlib;
+
+import edu.wpi.first.math.util.Units;
+
+public class Library {
+    
+    public static double someLibrarymethod() {
+        return Units.degreesToRadians(4);
+    }
+}
