f5a88bf64af34c1d1af5771e86e1bcc10ddc5dda
Jeff Morris <ffejcsirrom@gmail.com>
2022-07-02 11:13:15 -0500

    general updates

---
+++ src/main/java/io/github/tigerbotics7125/tigerlib/util/CleanSupplier.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 Tigerbotics and it's members. All rights reserved.
+ * This work is licensed under the terms of the GNU GPLv3 license
+ * found in the root directory of this project.
+ */
+package io.github.tigerbotics7125.tigerlib.util;
+
+import java.util.function.Supplier;
+
+/**
+ * A Supplier which returns cleansed input.
+ *
+ * @author Jeffrey Morris | Tigerbotics 7125
+ */
+@FunctionalInterface
+public interface CleanSupplier<T> extends Supplier<T> {
+
+    public abstract T get();
+}
