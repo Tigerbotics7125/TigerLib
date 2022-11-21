a192fe0328c651e3db7b3f4940f4f7ef2f26c447
Jeff Morris <ffejcsirrom@gmail.com>
2022-11-21 10:53:40 -0600

    Convert WPILib Trigger usage to TigerLib

--- src/main/java/io/github/tigerbotics7125/tigerlib/input/JoystickTrigger.java
+++ src/main/java/io/github/tigerbotics7125/tigerlib/input/JoystickTrigger.java
@@ -6,7 +6,6 @@
 package io.github.tigerbotics7125.tigerlib.input;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * A {@link Trigger} wrapper for Joystick buttons.
@@ -14,21 +13,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * @author Jeffrey Morris | Tigerbotics 7125
  */
 public class JoystickTrigger extends Trigger {
-    private final GenericHID mJoystick;
-    private final int mButton;
 
     /**
      * @param joystick The HID device to read the button from.
      * @param button The button index to read from.
      */
     public JoystickTrigger(GenericHID joystick, int button) {
-        mJoystick = joystick;
-        mButton = button;
-    }
-
-    /** @return The value of the joystick's button. */
-    @Override
-    public boolean getAsBoolean() {
-        return mJoystick.getRawButton(mButton);
+        super(() -> joystick.getRawButton(button));
     }
 }
