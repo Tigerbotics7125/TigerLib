ea8646a7a00e6265cb00b505dec8f18d55b9d337
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-01-18 12:08:44 -0600

    update Conversion to be more understandable

--- src/main/java/io/github/tigerbotics7125/tigerlib/math/Conversion.java
+++ src/main/java/io/github/tigerbotics7125/tigerlib/math/Conversion.java
@@ -9,59 +9,58 @@ import java.util.function.Function;
 
 /**
  * Represent a conversion factor from any measurement to another.
- * <p>
- * For instance you can have a {@code Conversion<Integer, Double>} which could
- * convert between encoder counts and degrees.
+ *
+ * <p>For instance you can have a {@code Conversion<Double, Double>} which could convert between
+ * motor RPM to wheel MPS
  *
  * @author Jeffrey Morris | Tigerbotics 7125
  * @author Myles Pasetsky | StuyPulse 694
  */
-public interface Conversion<From, To> {
+public interface Conversion<Input, Output> {
 
     /**
-     * Create a {@link Conversion} object.
+     * Creates a {@code Conversion<I, O>} object.
      *
-     * @param <In>  Input
-     * @param <Out> Output
-     * @param to    Function to convert input to output.
-     * @param from  Function to produce output from input.
-     * @return A {@link Conversion} object representing an ability to convert
-     *         between input and output.
+     * @param <I> Input class type.
+     * @param <O> Output class type.
+     * @param inputToOutput {@code Function<I, O>} which converts input measurement to output
+     *     measurement.
+     * @param outputToInput {@code Function<O, I>} which converts output measurement to input
+     *     measurement.
+     * @return A {@link Conversion} object representing the two functions.
      */
-    static <In, Out> Conversion<In, Out> create(Function<In, Out> to, Function<Out, In> from) {
-	return new Conversion<>() {
-	    public Out to(In value) {
-		return to.apply(value);
-	    }
+    public static <I, O> Conversion<I, O> create(
+            Function<I, O> inputToOutput, Function<O, I> outputToInput) {
+        return new Conversion<>() {
+            @Override
+            public O fromInput(I input) {
+                return inputToOutput.apply(input);
+            }
 
-	    public In from(Out value) {
-		return from.apply(value);
-	    }
-	};
+            @Override
+            public I fromOutput(O output) {
+                return outputToInput.apply(output);
+            }
+        };
     }
 
     /**
-     * Invert this {@link Conversion}.
-     *
-     * @return This conversion represented as {@code Conversion<To, From>}.
+     * @return A new {@link Conversion} object which flips the input and output of this object such
+     *     that {@code Conversion<I, O>.invert()} would return {@code Conversion<O, I>}.
      */
-    default Conversion<To, From> invert() {
-	return create(this::from, this::to);
+    default Conversion<Output, Input> invert() {
+        return create(this::fromOutput, this::fromInput);
     }
 
     /**
-     * Converts input to output.
-     *
-     * @param from The input.
-     * @return The output.
+     * @param input Measurement representing type {@code Input} to convert to type {@code Output}.
+     * @return Type {@code Output} converted from {@code Input}.
      */
-    To to(From from);
+    public Output fromInput(Input input);
 
     /**
-     * Produces output from input.
-     *
-     * @param to The output.
-     * @return The input.
+     * @param output Measurement representing type {@code Output} to convert to type {@code Input}.
+     * @return Type {@code Input} converted from {@code Output}.
      */
-    From from(To to);
+    public Input fromOutput(Output output);
 }
