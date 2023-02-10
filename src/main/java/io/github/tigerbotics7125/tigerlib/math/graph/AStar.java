1200cd783f552026dff997da494da4536933ddfb
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-02-09 23:29:26 -0600

    update license dates

--- src/main/java/io/github/tigerbotics7125/tigerlib/math/graph/AStar.java
+++ src/main/java/io/github/tigerbotics7125/tigerlib/math/graph/AStar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Tigerbotics and it's members. All rights reserved.
+ * Copyright (c) 2023 Tigerbotics and it's members. All rights reserved.
  * This work is licensed under the terms of the GNU GPLv3 license
  * found in the root directory of this project.
  */
